7394601824def6eb5a4e7ab06a7bc17698d08e04
zyj16602159899 <z16602159899@163.com>
2019-10-28 19:16:42 +0800

    class_1027

---
+++ class_1027/class_practice.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+#@Author：zhuxiujie
+
+#打印九九乘法表
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print('{}*{}={}\t'.format(j,i,j*i),end='')
+#     print('\n')
+
+# for i in range(1, 10):
+#     print()
+#     for j in range(1, i+1):
+#         print("%d*%d=%d" % (j, i, i*j))
+
+#有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+# for i in range(1,5):
+#     for j in range(1,5):
+#         for k in range(1,5):
+#             if i != j and i != k and j != k:
+#                 print(i,'',j,'',k)
+
+#输入三个整数x,y,z，请把这三个数由小到大输出
+# list_01 = []
+# for i in range(3):
+#     x = int(input('请输入整数：'))
+#     list_01.append(x)
+# list_01.sort()
+# print(list_01)
+
+#斐波那契数列
+# def fib(n):
+#     a,b = 1,1
+#     for i in range(n-1):
+#         a,b = b,a+b
+#     return a
+#
+# print(fib(8))
+
+# l = [1,2,3,4]
+# ll = l[::-1]
+# print(ll)
+
+# 判断101-200之间有多少个素数，并列表输出所有素数
+# l = []
+# for i in range(101,200):
+#     for j in range(2,i):
+#         if i%j == 0:
+#             break
+#     else:
+#         # print('{}是素数'.format(i))
+#         l.append(i)
+# print(l)
+
+# 打印出所有的"水仙花数"
+# 所谓"水仙花数"是指一个三位数，其各位数字立方和等于该数本身
+for n in range(100,1000):
+    i = n//100
+    j = n//10%10
+    k = n%10
+    if n == i**3 + j**3 + k**3:
+        print(n)
+
+print(888//100)
--- class_1027/python_function.py
+++ class_1027/python_function.py
@@ -24,9 +24,32 @@ add_numbers()
 #第二步：变成函数，加def
 #第三步：想办法提高代码的复用性
 
-def add_numbers(m,n,k=1):
+#默认参数，必须要放在位置参数的后面
+def add_numbers(m,n,k=1):   #形参/位置参数，k=1是位置参数
     sum = 0
     for i in range(m,n,k):
         sum += i
     print("求和值是：{}".format(sum))
-add_numbers(1,101)
+add_numbers(1,101)  #实参，默认情况下按顺序赋值
+
+#return: 当你调用函数时，就会返回一个值，即return后面的表达式
+#在函数中相当于一个结束符号，表示函数到此为止，后面的代码不会被执行
+#什么时候用：当需要调用函数值的时候用return
+def add_numbers(m,n,k=1):
+    sum = 0
+    for i in range(m,n,k):
+        sum += i
+    return sum
+
+result = add_numbers(1,10)
+print(result)
+
+#写函数，检查传入列表的长度，如果大于2，那么仅仅保留前两个长度的内容，并将新内容返回
+def check_list(L):
+    if len(L) > 2:
+        new_list = L[0:2]
+    return new_list
+L = [1,2,3]
+print(check_list(L))
+
+#动态参数/不定长参数ix
