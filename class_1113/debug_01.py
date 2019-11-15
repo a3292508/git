a0c31d07acae4f86eedb9e391b9741483c9f96ec
zyj16602159899 <z16602159899@163.com>
2019-11-15 18:04:37 +0800

    1115

---
+++ class_1113/debug_01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+#@Author：zhuxiujie
+
+# debug
+# step Over:到下一行
+# step Into:到代码里面去
+# Force Step Into:强制进入到函数内部去
+# Step Out:跳出函数
+
+#什么时候用：出错的时候，哪行出错了，就在哪行debug
